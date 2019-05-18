0830c1748d1d1ee3ce7c9752e0a335bde12bd971
stdcoutzrh <stdcoutzrh@gmail.com>
2019-05-18 23:23:27 +0800

    del sth unused

--- mpp/RtspIPC/common/rtsp.c
+++ mpp/RtspIPC/common/rtsp.c
@@ -14,22 +14,18 @@ pthread_cond_t  g_cond;
 pthread_mutex_t g_sendmutex;
 
 pthread_t g_SendDataThreadId = 0;
-//HAL_CLIENT_HANDLE hMainStreamClient = NULL,hSubStreamClient = NULL,hAudioClient = NULL;
 char g_rtp_playload[20];
 int   g_audio_rate = 8000;
-//VIDEO_NORM_E gs_enNorm = VIDEO_ENCODING_MODE_NTSC;//30fps
 int g_nframerate;
 
 int exitok = 0;
 int udpfd;
 int count=0;
 
-
 struct list_head RTPbuf_head = LIST_HEAD_INIT(RTPbuf_head);
-
 static pthread_t gs_RtpPid;
 
-
+//get date
 static char const* dateHeader()
 {
 	static char buf[200];
@@ -40,6 +36,8 @@ static char const* dateHeader()
 
 	return buf;
 }
+
+//get local ip
 static char* GetLocalIP(int sock)
 {
 	struct ifreq ifreq;
@@ -51,7 +49,6 @@ static char* GetLocalIP(int sock)
 		sin = (struct sockaddr_in *)&ifreq.ifr_addr;
 		sin->sin_family = AF_INET;
        	strcpy(LocalIP,inet_ntoa(sin->sin_addr)); 
-		//inet_ntop(AF_INET, &sin->sin_addr,LocalIP, 16);
     	} 
 	printf("--------------------------------------------%s\n",LocalIP);
 	return LocalIP;
@@ -62,121 +59,122 @@ char* strDupSize(char const* str)
   if (str == NULL) return NULL;
   size_t len = strlen(str) + 1;
   char* copy = malloc(len);
-
   return copy;
 }
 
-int ParseRequestString(char const* reqStr,
-		       unsigned reqStrSize,
-		       char* resultCmdName,
-		       unsigned resultCmdNameMaxSize,
-		       char* resultURLPreSuffix,
-		       unsigned resultURLPreSuffixMaxSize,
-		       char* resultURLSuffix,
-		       unsigned resultURLSuffixMaxSize,
-		       char* resultCSeq,
-		       unsigned resultCSeqMaxSize) 
+int ParseRequestString(char const* reqStr,unsigned reqStrSize,
+		       char* resultCmdName,unsigned resultCmdNameMaxSize,
+		       char* resultURLPreSuffix,unsigned resultURLPreSuffixMaxSize,
+		       char* resultURLSuffix,unsigned resultURLSuffixMaxSize,
+		       char* resultCSeq,unsigned resultCSeqMaxSize) 
 {
-  // This parser is currently rather dumb; it should be made smarter #####
-
-  // Read everything up to the first space as the command name:
-  int parseSucceeded = FALSE;
-  unsigned i;
-  for (i = 0; i < resultCmdNameMaxSize-1 && i < reqStrSize; ++i) {
-    char c = reqStr[i];
-    if (c == ' ' || c == '\t') {
-      parseSucceeded = TRUE;
-      break;
-    }
-
-    resultCmdName[i] = c;
-  }
-  resultCmdName[i] = '\0';
-  if (!parseSucceeded) return FALSE;
-      
-  // Skip over the prefix of any "rtsp://" or "rtsp:/" URL that follows:
-  unsigned j = i+1;
-  while (j < reqStrSize && (reqStr[j] == ' ' || reqStr[j] == '\t')) ++j; // skip over any additional white space
-  for (j = i+1; j < reqStrSize-8; ++j) {
-    if ((reqStr[j] == 'r' || reqStr[j] == 'R')
-	&& (reqStr[j+1] == 't' || reqStr[j+1] == 'T')
-	&& (reqStr[j+2] == 's' || reqStr[j+2] == 'S')
-	&& (reqStr[j+3] == 'p' || reqStr[j+3] == 'P')
-	&& reqStr[j+4] == ':' && reqStr[j+5] == '/') {
-      j += 6;
-      if (reqStr[j] == '/') {
-	// This is a "rtsp://" URL; skip over the host:port part that follows:
-	++j;
-	while (j < reqStrSize && reqStr[j] != '/' && reqStr[j] != ' ') ++j;
-      } else {
-	// This is a "rtsp:/" URL; back up to the "/":
-	--j;
-      }
+	// This parser is currently rather dumb; it should be made smarter #####
+	// Read everything up to the first space as the command name:
+	int parseSucceeded = FALSE;
+	unsigned i;
+	for (i = 0; i < resultCmdNameMaxSize-1 && i < reqStrSize; ++i) 
+	{
+		char c = reqStr[i];
+		if (c == ' ' || c == '\t')
+		{
+			parseSucceeded = TRUE;
+			break;
+		}
+		resultCmdName[i] = c;
+	}
+	resultCmdName[i] = '\0';
+	if (!parseSucceeded) return FALSE;     
+	// Skip over the prefix of any "rtsp://" or "rtsp:/" URL that follows:
+	unsigned j = i+1;
+	while (j < reqStrSize && (reqStr[j] == ' ' || reqStr[j] == '\t')) ++j; // skip over any additional white space
+	for (j = i+1; j < reqStrSize-8; ++j) 
+	{
+		if ((reqStr[j] == 'r' || reqStr[j] == 'R')&& (reqStr[j+1] == 't' || reqStr[j+1] == 'T')
+				&& (reqStr[j+2] == 's' || reqStr[j+2] == 'S')&& (reqStr[j+3] == 'p' || reqStr[j+3] == 'P')
+				&& reqStr[j+4] == ':' && reqStr[j+5] == '/') 
+		{
+			j += 6;
+			if (reqStr[j] == '/') 
+			{
+				// This is a "rtsp://" URL; skip over the host:port part that follows:
+				++j;
+				while (j < reqStrSize && reqStr[j] != '/' && reqStr[j] != ' ') 
+					++j;
+			} 
+			else 
+			{
+				// This is a "rtsp:/" URL; back up to the "/":
+				--j;
+			}
       i = j;
       break;
-    }
-  }
-
-  // Look for the URL suffix (before the following "RTSP/"):
-  parseSucceeded = FALSE;
-  unsigned k;
-  for (k = i+1; k < reqStrSize-5; ++k) {
-    if (reqStr[k] == 'R' && reqStr[k+1] == 'T' &&
-	reqStr[k+2] == 'S' && reqStr[k+3] == 'P' && reqStr[k+4] == '/') {
-      while (--k >= i && reqStr[k] == ' ') {} // go back over all spaces before "RTSP/"
-      unsigned k1 = k;
-      while (k1 > i && reqStr[k1] != '/' && reqStr[k1] != ' ') --k1;
-      // the URL suffix comes from [k1+1,k]
-
-      // Copy "resultURLSuffix":
-      if (k - k1 + 1 > resultURLSuffixMaxSize) return FALSE; // there's no room
-      unsigned n = 0, k2 = k1+1;
-      while (k2 <= k) resultURLSuffix[n++] = reqStr[k2++];
-      resultURLSuffix[n] = '\0';
-
-      // Also look for the URL 'pre-suffix' before this:
-      unsigned k3 = --k1;
-      while (k3 > i && reqStr[k3] != '/' && reqStr[k3] != ' ') --k3;
-      // the URL pre-suffix comes from [k3+1,k1]
-
-      // Copy "resultURLPreSuffix":
-      if (k1 - k3 + 1 > resultURLPreSuffixMaxSize) return FALSE; // there's no room
-      n = 0; k2 = k3+1;
-      while (k2 <= k1) resultURLPreSuffix[n++] = reqStr[k2++];
-      resultURLPreSuffix[n] = '\0';
-
-      i = k + 7; // to go past " RTSP/"
-      parseSucceeded = TRUE;
-      break;
-    }
-  }
-  if (!parseSucceeded) return FALSE;
-
-  // Look for "CSeq:", skip whitespace,
-  // then read everything up to the next \r or \n as 'CSeq':
-  parseSucceeded = FALSE;
-  for (j = i; j < reqStrSize-5; ++j) {
-    if (reqStr[j] == 'C' && reqStr[j+1] == 'S' && reqStr[j+2] == 'e' &&
-	reqStr[j+3] == 'q' && reqStr[j+4] == ':') {
-      j += 5;
-      unsigned n;
-      while (j < reqStrSize && (reqStr[j] ==  ' ' || reqStr[j] == '\t')) ++j;
-      for (n = 0; n < resultCSeqMaxSize-1 && j < reqStrSize; ++n,++j) {
-	char c = reqStr[j];
-	if (c == '\r' || c == '\n') {
-	  parseSucceeded = TRUE;
-	  break;
+		}
 	}
 
-	resultCSeq[n] = c;
-      }
-      resultCSeq[n] = '\0';
-      break;
-    }
-  }
-  if (!parseSucceeded) return FALSE;
+	// Look for the URL suffix (before the following "RTSP/"):
+	parseSucceeded = FALSE;
+	unsigned k;
+	for (k = i+1; k < reqStrSize-5; ++k) 
+	{
+		if (reqStr[k] == 'R' && reqStr[k+1] == 'T' &&reqStr[k+2] == 'S' && reqStr[k+3] == 'P' && reqStr[k+4] == '/') 
+		{
+			while (--k >= i && reqStr[k] == ' ') {} // go back over all spaces before "RTSP/"
+			unsigned k1 = k;
+			while (k1 > i && reqStr[k1] != '/' && reqStr[k1] != ' ') --k1;
+			// the URL suffix comes from [k1+1,k]
+			// Copy "resultURLSuffix":
+			if (k - k1 + 1 > resultURLSuffixMaxSize) 
+				return FALSE; // there's no room
+			unsigned n = 0, k2 = k1+1;
+			while (k2 <= k) 
+				resultURLSuffix[n++] = reqStr[k2++];
+			resultURLSuffix[n] = '\0';
+			// Also look for the URL 'pre-suffix' before this:
+			unsigned k3 = --k1;
+			while (k3 > i && reqStr[k3] != '/' && reqStr[k3] != ' ') 
+				--k3;
+			// the URL pre-suffix comes from [k3+1,k1]
+			// Copy "resultURLPreSuffix":
+			if (k1 - k3 + 1 > resultURLPreSuffixMaxSize) 
+				return FALSE; // there's no room
+			n = 0; k2 = k3+1;
+			while (k2 <= k1) 
+				resultURLPreSuffix[n++] = reqStr[k2++];
+			resultURLPreSuffix[n] = '\0';
 
-  return TRUE;
+			i = k + 7; // to go past " RTSP/"
+			parseSucceeded = TRUE;
+			break;
+		}
+	}
+	if (!parseSucceeded) return FALSE;
+	// Look for "CSeq:", skip whitespace,
+	// then read everything up to the next \r or \n as 'CSeq':
+	parseSucceeded = FALSE;
+	for (j = i; j < reqStrSize-5; ++j) 
+	{
+		if(reqStr[j] == 'C'&& reqStr[j+1] =='S'&&reqStr[j+2]=='e'&&reqStr[j+3] =='q'&&reqStr[j+4]==':') 
+		{
+			j += 5;
+			unsigned n;
+			while (j < reqStrSize && (reqStr[j] ==  ' ' || reqStr[j] == '\t')) 
+				++j;
+			for (n = 0; n < resultCSeqMaxSize-1 && j < reqStrSize; ++n,++j) 
+			{
+				char c = reqStr[j];
+				if (c == '\r' || c == '\n') 
+				{
+					parseSucceeded = TRUE;
+					break;
+				}
+				resultCSeq[n] = c;
+			}
+			resultCSeq[n] = '\0';
+			break;
+		}
+	}
+	if (!parseSucceeded) return FALSE;
+	return TRUE;
 }
 
 int OptionAnswer(char *cseq, int sock)
@@ -228,16 +226,13 @@ int DescribeAnswer(char *cseq,int sock,char * urlSuffix,char* recvbuf)
 		pTemp2 += sprintf(pTemp2,"t=0 0\r\n");
 		pTemp2 += sprintf(pTemp2,"a=control:*\r\n");
 		
-
 		/*H264 TrackID=0 RTP_PT 96*/
 		pTemp2 += sprintf(pTemp2,"m=video 0 RTP/AVP 96\r\n");
 		pTemp2 += sprintf(pTemp2,"a=control:trackID=0\r\n");
 		pTemp2 += sprintf(pTemp2,"a=rtpmap:96 H264/90000\r\n");
 		pTemp2 += sprintf(pTemp2,"a=fmtp:96 packetization-mode=1; sprop-parameter-sets=%s\r\n", "AAABBCCC");
-#if 1
-		
-		/*G726*/
-		
+#if 1		
+		/*G726*/		
 		pTemp2 += sprintf(pTemp2,"m=audio 0 RTP/AVP 97\r\n");
 		pTemp2 += sprintf(pTemp2,"a=control:trackID=1\r\n");
 		if(strcmp(g_rtp_playload,"AAC")==0)
@@ -253,11 +248,9 @@ int DescribeAnswer(char *cseq,int sock,char * urlSuffix,char* recvbuf)
 #endif
 		pTemp += sprintf(pTemp,"Content-length: %d\r\n", strlen(sdpMsg));     
 		pTemp += sprintf(pTemp,"Content-Base: rtsp://%s/%s/\r\n\r\n",localip,urlSuffix);
-		
-		//printf("mem ready\n");
+
 		strcat(pTemp, sdpMsg);
 		free(localip);
-		//printf("Describe ready sent\n");
 		int re = send(sock, buf, strlen(buf),0);
 		if(re <= 0)
 		{
@@ -278,7 +271,7 @@ void ParseTransportHeader(char const* buf,StreamingMode* streamingMode,
 						 portNumBits* clientRTCPPortNum, // if UDP
 						 unsigned char* rtpChannelId, // if TCP
 						 unsigned char* rtcpChannelId )// if TCP)
- {
+{
 	// Initialize the result parameters to default values:
 	*streamingMode = RTP_UDP;
 	*streamingModeString = NULL;
@@ -352,8 +345,6 @@ int SetupAnswer(char *cseq,int sock,int SessionId,char * urlSuffix,char* recvbuf
 			&clientRTPPortNum, &clientRTCPPortNum,
 			&rtpChannelId, &rtcpChannelId);
 
-		//Port clientRTPPort(clientRTPPortNum);
-		//Port clientRTCPPort(clientRTCPPortNum);
 		*rtpport = clientRTPPortNum;
 		*rtcpport = clientRTCPPortNum;
 		
@@ -477,7 +468,6 @@ void * RtspClientMsg(void*pParam)
 	while(pClient->status != RTSP_IDLE)
 	{
 		nRes = recv(pClient->socket, pRecvBuf, RTSP_RECV_SIZE,0);
-		//printf("-------------------%d\n",nRes);
 		if(nRes < 1)
 		{
 			//usleep(1000);
@@ -500,10 +490,6 @@ void * RtspClientMsg(void*pParam)
 		char *p = pRecvBuf;
 
 		printf("<<<<<%s\n",p);
-
-		//printf("\--------------------------\n");
-		//printf("%s %s\n",urlPreSuffix,urlSuffix);
-
 		if(strstr(cmdName, "OPTIONS"))
 		{
 			OptionAnswer(cseq,pClient->socket);
@@ -512,8 +498,6 @@ void * RtspClientMsg(void*pParam)
 		else if(strstr(cmdName, "DESCRIBE"))
 		{
 			DescribeAnswer(cseq,pClient->socket,urlSuffix,p);
-			//printf("-----------------------------DescribeAnswer %s %s\n",
-			//	urlPreSuffix,urlSuffix);
 		}
 		else if(strstr(cmdName, "SETUP"))
 		{
@@ -522,23 +506,18 @@ void * RtspClientMsg(void*pParam)
 			SetupAnswer(cseq,pClient->socket,pClient->sessionid,urlPreSuffix,p,&rtpport,&rtcpport);
 
 			sscanf(urlSuffix, "trackID=%u", &trackID);
-			//printf("----------------------------------------------TrackId %d\n",trackID);
 			if(trackID<0 || trackID>=2)trackID=0;
 			g_rtspClients[pClient->index].rtpport[trackID] = rtpport;
 			g_rtspClients[pClient->index].rtcpport= rtcpport;
 			g_rtspClients[pClient->index].reqchn = atoi(urlPreSuffix);
 			if(strlen(urlPreSuffix)<100)
 				strcpy(g_rtspClients[pClient->index].urlPre,urlPreSuffix);
-			//printf("-----------------------------SetupAnswer %s-%d-%d\n",
-			//	urlPreSuffix,g_rtspClients[pClient->index].reqchn,rtpport);
 		}
 		else if(strstr(cmdName, "PLAY"))
 		{
 			PlayAnswer(cseq,pClient->socket,pClient->sessionid,g_rtspClients[pClient->index].urlPre,p);
 			g_rtspClients[pClient->index].status = RTSP_SENDING;
 			printf("Start Play\n",pClient->index);
-			//printf("-----------------------------PlayAnswer %d %d\n",pClient->index);
-			//usleep(100);
 		}
 		else if(strstr(cmdName, "PAUSE"))
 		{
@@ -553,7 +532,6 @@ void * RtspClientMsg(void*pParam)
 			g_rtspClients[pClient->index].tsaud = 0;
 			close(pClient->socket);
 		}
-		//if(exitok){ exitok++;return NULL; } 
 	}
 	printf("RTSP:-----Exit Client %s\n",pClient->IP);
 	return NULL;
@@ -601,7 +579,7 @@ void * RtspServerListen(void*pParam)
     {
 		printf("<<<<RTSP Client %s Connected...\n", inet_ntoa(addrAccept.sin_addr));
 
-		int nMaxBuf = 10 * 1024; // ÏµÍ³œ«»á·ÖÅä 2 x nMaxBuf µÄ»º³åŽóÐ¡
+		int nMaxBuf = 10 * 1024; 
 		if(setsockopt(s32CSocket, SOL_SOCKET, SO_SNDBUF, (char*)&nMaxBuf, sizeof(nMaxBuf)) == -1)
 			printf("RTSP:!!!!!! Enalarge socket sending buffer error !!!!!!\n");
 		int i;
@@ -620,10 +598,7 @@ void * RtspServerListen(void*pParam)
 				
 				struct sched_param sched;
 				sched.sched_priority = 1;
-				//to return ACKecho
-				pthread_create(&threadIdlsn, NULL, RtspClientMsg, &g_rtspClients[i]);
-				//pthread_setschedparam(threadIdlsn,SCHED_RR,&sched);
-				
+				pthread_create(&threadIdlsn, NULL, RtspClientMsg, &g_rtspClients[i]);				
 				bAdd = TRUE;
 				break;
 			}
@@ -639,22 +614,18 @@ void * RtspServerListen(void*pParam)
 			pthread_t threadIdlsn = 0;
 			struct sched_param sched;
 			sched.sched_priority = 1;
-			//to return ACKecho
 			pthread_create(&threadIdlsn, NULL, RtspClientMsg, &g_rtspClients[0]);
-			//pthread_setschedparam(threadIdlsn,SCHED_RR,&sched);
 			bAdd = TRUE;
-		}
-		//if(exitok){ exitok++;return NULL; } 	
+		}		
     }
     if(s32CSocket < 0)
     {
        // HI_OUT_Printf(0, "RTSP listening on port %d,accept err, %d\n", RTSP_SERVER_PORT, s32CSocket);
     }
-
-	printf("----- INIT_RTSP_Listen() Exit !! \n");
-	
+	printf("----- INIT_RTSP_Listen() Exit !! \n");	
 	return NULL;
 }
+
 HI_S32 VENC_Sent(char *buffer,int buflen)
 {
     HI_S32 i;
@@ -790,9 +761,6 @@ HI_S32 VENC_Sent(char *buffer,int buflen)
 
 	//------------------------------------------------------------
 }
-/******************************************************************************
-* funciton : sent H264 stream
-******************************************************************************/
 
 HI_S32 SAMPLE_COMM_VENC_Sentjin(VENC_STREAM_S *pstStream)
 {
@@ -845,11 +813,9 @@ HI_S32 saveStream(VENC_STREAM_S *pstStream)
 
 				list_add_tail(&(p->list),&RTPbuf_head);
 				count++;
-				//printf("count = %d\n",count);
 		    }
     	}
     }
-
     return HI_SUCCESS;
 }
 
@@ -865,20 +831,14 @@ void RtspServer_init(void)
 	pthread_mutex_init(&g_mutex,NULL);
 	pthread_cond_init(&g_cond,NULL);
 	memset(&g_rtspClients,0,sizeof(RTSP_CLIENT)*MAX_RTSP_CLIENT);
-	
-	//pthread_create(&g_SendDataThreadId, NULL, SendDataThread, NULL);
-	
+
 	struct sched_param thdsched;
 	thdsched.sched_priority = 2;
-	//to listen visiting
+
 	pthread_create(&threadId, NULL, RtspServerListen, NULL);
-	//pthread_setschedparam(threadId,SCHED_RR,&thdsched);
 	printf("RTSP:-----Init Rtsp server\n");
 
 	pthread_create(&gs_RtpPid, 0, vdRTPSendThread, NULL);
-	
-	//exitok++;
-
 }
 void RtspServer_exit(void)
 {
@@ -898,9 +858,7 @@ HI_VOID* vdRTPSendThread(HI_VOID *p)
 			free(p->buf);
 			free(p);
 			p = NULL;
-			count--;
-			//printf("count = %d\n",count);
-		
+			count--;	
 		}
 		usleep(5000);
 	}
